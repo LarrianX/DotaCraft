cef166e2bdd7e2e91e23fe3e747abe1219fb5a9d
Youl <litserg2017@gmail.com>
2024-09-30 00:15:41 +0500

    Yayay Tango on tree!!

--- src/main/java/com/dota2/item/Tango.java
+++ src/main/java/com/dota2/item/Tango.java
@@ -3,6 +3,7 @@ package com.dota2.item;
 import com.dota2.component.EffectComponent;
 import com.dota2.effect.ModEffects;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
@@ -12,14 +13,14 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-
-import java.util.Map;
 
 import static com.dota2.component.ModComponents.EFFECT_COMPONENT;
 
-public class
-Tango extends Item implements CustomItem {
+public class Tango extends Item implements CustomItem {
     public static final String FULLNESS_KEY = "fullness";
     public static final int MAX_FULLNESS = 3;
     private static final String ID = "tango";
@@ -39,12 +40,9 @@ Tango extends Item implements CustomItem {
     }
 
     @Override
-    public void inventoryTick(ItemStack stack, net.minecraft.world.World world, Entity entity, int slot, boolean selected) {
-        // inventoryTick - функция вызывается каждый тик в инвентаре
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         super.inventoryTick(stack, world, entity, slot, selected);
-        // Получаем nbt из стака
         NbtCompound nbt = stack.getOrCreateNbt();
-        // Если у предмета нет в nbt нашего fullness - создаём fullness и ставим максимум
         if (!nbt.contains(FULLNESS_KEY) || nbt.getInt(FULLNESS_KEY) > MAX_FULLNESS) {
             nbt.putInt(FULLNESS_KEY, MAX_FULLNESS);
         }
@@ -52,26 +50,44 @@ Tango extends Item implements CustomItem {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        // Получаем стак
         ItemStack stack = user.getStackInHand(hand);
-        // Получаем NBT и значение fullness
         NbtCompound nbt = stack.getOrCreateNbt();
         int fullness = nbt.getInt(FULLNESS_KEY);
 
         if (fullness == 0) {
-            // Такая ситуация невозможна без команд, но, why not
             return TypedActionResult.fail(stack);
         }
 
-        // Выполняем действия
-        applyEffects(user);
+        // Проверка, на что нацелился игрок (дерево)
+        BlockHitResult hitResult = (BlockHitResult) user.raycast(5.0D, 0.0F, false);
+        if (hitResult.getType() == BlockHitResult.Type.BLOCK && world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.OAK_LOG) {
 
-        // Проверяем режим игры игрока и обновляем стак
-        if (!user.isCreative()) {
-            updateStack(stack, nbt, fullness);
+            // Удаление всего дерева
+            removeTree(world, hitResult.getBlockPos());
+
+            applyEffects(user);
+
+            if (!user.isCreative()) {
+                updateStack(stack, nbt, fullness);
+            }
+
+            return TypedActionResult.success(stack);
         }
 
-        return TypedActionResult.success(stack);
+        return TypedActionResult.fail(stack);
+    }
+
+    private void removeTree(World world, BlockPos pos) {
+        // Проверка, является ли блок древесиной
+        if (world.getBlockState(pos).getBlock() == Blocks.OAK_LOG || world.getBlockState(pos).getBlock() == Blocks.OAK_LEAVES) {
+            world.setBlockState(pos, Blocks.AIR.getDefaultState());
+
+            // Рекурсивный вызов для проверки всех соседних блоков
+            for (Direction direction : Direction.values()) {
+                BlockPos neighborPos = pos.offset(direction);
+                removeTree(world, neighborPos);
+            }
+        }
     }
 
     private void applyEffects(PlayerEntity user) {
@@ -82,7 +98,6 @@ Tango extends Item implements CustomItem {
         component.getAmplifiers().put(ModEffects.REGENERATION_HEALTH.getId(), 0.35 + ERROR);
         component.sync();
     }
-
 
     private void updateStack(ItemStack stack, NbtCompound nbt, int fullness) {
         if (fullness > 1) {
