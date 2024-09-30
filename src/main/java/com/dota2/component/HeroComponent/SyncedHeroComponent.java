5cc7179651b6d9962141178ef62806f5235a82d4
LarrianX <linar.habibutdinov@yandex.ru>
2024-09-30 23:03:32 +0500

    сделал урон

--- src/main/java/com/dota2/component/HeroComponent/SyncedHeroComponent.java
+++ src/main/java/com/dota2/component/HeroComponent/SyncedHeroComponent.java
@@ -1,10 +1,23 @@
 package com.dota2.component.HeroComponent;
 
 import com.dota2.component.ModComponents;
+import com.dota2.item.Weapon;
 import dev.onyxstudios.cca.api.v3.component.sync.AutoSyncedComponent;
 import dev.onyxstudios.cca.api.v3.component.tick.ServerTickingComponent;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import static com.dota2.component.ModComponents.HERO_COMPONENT;
+import static com.dota2.component.ModComponents.VALUES_COMPONENT;
 
 public class SyncedHeroComponent implements HeroComponent, ServerTickingComponent, AutoSyncedComponent {
     public static final int HEALTH = 100;
@@ -13,11 +26,41 @@ public class SyncedHeroComponent implements HeroComponent, ServerTickingComponen
 
     public SyncedHeroComponent(PlayerEntity provider) {
         this.provider = provider;
+        AttackEntityCallback.EVENT.register(this::onAttackEntity);
+    }
+
+    private int calculateDamage() {
+        int totalDamage = 0;
+
+        for (int i = 0; i < this.provider.getInventory().size(); i++) {
+            ItemStack stack = this.provider.getInventory().getStack(i);
+
+            if (stack.getItem() instanceof Weapon weapon) {
+                totalDamage += weapon.getDamage();
+            }
+        }
+
+        return totalDamage;
+    }
+
+    private ActionResult onAttackEntity(PlayerEntity playerEntity, World world, Hand hand, Entity entity, @Nullable EntityHitResult entityHitResult) {
+        int damage = calculateDamage();
+        provider.sendMessage(Text.literal("Урон: " + damage), false);
+
+        if (this.hero && playerEntity.equals(provider) && entity instanceof PlayerEntity playerTarget) {
+            HeroComponent heroComponent = playerTarget.getComponent(HERO_COMPONENT);
+            if (heroComponent.isHero()) {
+                ValuesComponent valuesComponent = playerTarget.getComponent(VALUES_COMPONENT);
+                valuesComponent.addHealth(-50);
+                return ActionResult.SUCCESS;
+            }
+        }
+        return ActionResult.FAIL;
     }
 
     @Override
     public void sync() {
-        ModComponents.HERO_COMPONENT.sync(this.provider);
+        HERO_COMPONENT.sync(this.provider);
     }
 
     @Override
--- src/main/java/com/dota2/item/Weapon.java
+++ src/main/java/com/dota2/item/Weapon.java
@@ -9,22 +9,11 @@ import net.minecraft.item.SwordItem;
 
 import static com.dota2.component.ModComponents.VALUES_COMPONENT;
 
-public abstract class CustomSword extends SwordItem {
+public abstract class Weapon extends SwordItem {
     private static final float ATTACK_SPEED = 2.0f;
 
-    public CustomSword(int damage) {
+    public Weapon(int damage) {
         super(DotaMaterial.INSTANCE, damage, ATTACK_SPEED, new FabricItemSettings().maxCount(1));
-    }
-
-    @Override
-    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        if (target instanceof PlayerEntity player) {
-            ValuesComponent component = player.getComponent(VALUES_COMPONENT);
-
-            component.addHealth(-getDamage());
-            component.sync();
-        }
-        return super.postHit(stack, target, attacker);
     }
 
     public abstract int getDamage();
