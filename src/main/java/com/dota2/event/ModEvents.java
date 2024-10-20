9cb820fe90bbd6917e7c325df10232837139912f
MrTime4ik <MrTime4ik@gmail.com>
2024-10-20 18:33:48 +0300

    Все кроме героя теперь получают урон

--- src/main/java/com/dota2/event/ModEvents.java
+++ src/main/java/com/dota2/event/ModEvents.java
@@ -4,6 +4,8 @@ import com.dota2.mixin.PlayerEntityMixin;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.minecraft.entity.player.PlayerEntity;
 
+import static com.dota2.component.ModComponents.HERO_COMPONENT;
+
 public class ModEvents {
 
     public static void AllowDamage() {
@@ -11,11 +13,17 @@ public class ModEvents {
 
             if (entity.isPlayer()) {
                 PlayerEntity player = (PlayerEntity) entity;
-                player.setHealth(20);
-                player.setFireTicks(0);
-                player.setFrozenTicks(0);
+                if (player.getComponent(HERO_COMPONENT).isHero()) {
+                    player.setHealth(20);
+                    player.setFireTicks(0);
+                    player.setFrozenTicks(0);
+                    return false;
+                } else {
+                    return true;
+                }
+            } else {
+                return true;
             }
-            return false;
         }));
 
     }
