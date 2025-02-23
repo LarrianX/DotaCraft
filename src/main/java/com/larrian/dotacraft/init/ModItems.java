efce712ce39ad7ae383a45f1760a0040b1d5c68f
Youl <litserg2017@gmail.com>
2025-02-23 17:53:15 +0500

    Added template of heaven halberd

--- src/main/java/com/larrian/dotacraft/init/ModItems.java
+++ src/main/java/com/larrian/dotacraft/init/ModItems.java
@@ -2,21 +2,7 @@ package com.larrian.dotacraft.init;
 
 import com.larrian.dotacraft.Custom;
 import com.larrian.dotacraft.DotaCraft;
-import com.larrian.dotacraft.item.custom.BattleFuryItem;
-import com.larrian.dotacraft.item.custom.BootsOfSpeedItem;
-import com.larrian.dotacraft.item.custom.ClarityItem;
-import com.larrian.dotacraft.item.custom.CrystalysItem;
-import com.larrian.dotacraft.item.custom.DaedalusItem;
-import com.larrian.dotacraft.item.custom.DemonEdgeItem;
-import com.larrian.dotacraft.item.custom.FlaskItem;
-import com.larrian.dotacraft.item.custom.MangoItem;
-import com.larrian.dotacraft.item.custom.RecDaedalusItem;
-import com.larrian.dotacraft.item.custom.ShadowBladeItem;
-import com.larrian.dotacraft.item.custom.TpScrollItem;
-import com.larrian.dotacraft.item.custom.TangoItem;
-import com.larrian.dotacraft.item.custom.TangoTFItem;
-import com.larrian.dotacraft.item.custom.PhaseBootsItem;
-import com.larrian.dotacraft.item.custom.BottleItem;
+import com.larrian.dotacraft.item.custom.*;
 import com.larrian.dotacraft.item.rune.RuneDoubleDamageItem;
 import com.larrian.dotacraft.item.rune.RuneInvisibilityItem;
 import com.larrian.dotacraft.item.rune.RuneRegenerationItem;
@@ -30,7 +16,7 @@ import java.util.List;
 
 public class ModItems {
 
-    // Список всех зарегистрированных предметов
+    // List of all items
     public static final List<Item> ITEMS = new ArrayList<>();
 
     public static final FlaskItem FLASK = register(new FlaskItem());
@@ -54,6 +40,8 @@ public class ModItems {
     public static final RuneInvisibilityItem RUNE_INVISIBILITY = register(new RuneInvisibilityItem());
     public static final RuneRegenerationItem RUNE_REGENERATION = register(new RuneRegenerationItem());
     public static final PhaseBootsItem PHASE_BOOTS = register(new PhaseBootsItem());
+    public static final HeavenHalberdItem HEAVEN_HALBERD = register(new HeavenHalberdItem());
+
 
     private static <T extends Item> T register(T item) {
         if (item instanceof Custom) {
@@ -66,6 +54,5 @@ public class ModItems {
 
     public static void registerModItems() {
         DotaCraft.LOGGER.info("Registering Mod Items for " + DotaCraft.MOD_ID);
-        // Регистрация выполнена при инициализации статических полей
     }
 }
