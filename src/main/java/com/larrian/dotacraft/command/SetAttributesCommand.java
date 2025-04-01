4a8a50fe4aa1f83dde142132eb177cd5c8c4bea1
LarrianX <linar.habibutdinov@yandex.ru>
2025-04-01 16:57:22 +0500

    fix SetAttributesCommand

--- src/main/java/com/larrian/dotacraft/command/SetAttributesCommand.java
+++ src/main/java/com/larrian/dotacraft/command/SetAttributesCommand.java
@@ -1,14 +1,14 @@
 package com.larrian.dotacraft.command;
 
-import com.larrian.dotacraft.component.attributes.DotaAttributeType;
-import com.larrian.dotacraft.component.attributes.AttributesComponent;
+import com.larrian.dotacraft.attributes.DotaAttributeType;
+import com.larrian.dotacraft.component.AttributesComponent;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.DoubleArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
-import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -17,30 +17,26 @@ import net.minecraft.text.Text;
 import static com.larrian.dotacraft.init.ModComponents.ATTRIBUTES_COMPONENT;
 
 public class SetAttributesCommand {
-    private static final SuggestionProvider<ServerCommandSource> SUGGESTION_ATTRIBUTES = (context, builder) -> {
-        for (DotaAttributeType type : DotaAttributeType.values()) {
-            builder.suggest(type.name());
-        }
-        return builder.buildFuture();
-    };
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register(
-                CommandManager.literal("set")
-                        .then(CommandManager.argument("attribute", StringArgumentType.string())
-                                .suggests(SUGGESTION_ATTRIBUTES)
-                                .then(CommandManager.argument("value", DoubleArgumentType.doubleArg())
-                                        .executes(SetAttributesCommand::execute))));
+        LiteralArgumentBuilder<ServerCommandSource> command = CommandManager.literal("set");
+        for (DotaAttributeType type : DotaAttributeType.values()) {
+            command.then(
+                    CommandManager.literal(type.name())
+                            .then(CommandManager.argument("value", DoubleArgumentType.doubleArg())
+                                    .executes((context -> execute(context, type.name())))));
+        }
+
+        dispatcher.register(command);
     }
 
-    private static int execute(com.mojang.brigadier.context.CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+    private static int execute(CommandContext<ServerCommandSource> context, String attributeName) throws CommandSyntaxException {
         ServerCommandSource source = context.getSource();
         ServerPlayerEntity player = source.getPlayer();
         if (player == null) {
             throw new SimpleCommandExceptionType(Text.literal("This command can only be used by a player.")).create();
         }
 
-        String attributeName = StringArgumentType.getString(context, "attribute");
         double value = DoubleArgumentType.getDouble(context, "value");
 
         DotaAttributeType attributeType;
